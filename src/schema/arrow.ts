6fa8358bbe4275152d0310113ea9aa2005c87d5a
Kemal Hadimli <disq@users.noreply.github.com>
2023-08-08 14:40:26 +0100

    feat: Transformers

---
+++ src/schema/arrow.ts
@@ -0,0 +1,9 @@
+export const METADATA_UNIQUE = "cq:extension:unique"
+export const METADATA_PRIMARY_KEY = "cq:extension:primary_key"
+export const METADATA_CONSTRAINT_NAME = "cq:extension:constraint_name"
+export const METADATA_INCREMENTAL = "cq:extension:incremental"
+
+export const METADATA_TRUE = "true"
+export const METADATA_FALSE = "false"
+export const METADATA_TABLE_NAME = "cq:table_name"
+export const METADATA_TABLE_DESCRIPTION = "cq:table_description"
