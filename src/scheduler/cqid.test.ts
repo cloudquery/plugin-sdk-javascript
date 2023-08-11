d8474ce010f4be7dee11cfef785074cee8bbc5bf
Erez Rokah <erezrokah@users.noreply.github.com>
2023-08-11 18:58:41 +0200

    feat: Add UUID scalar (#54)

--- src/scheduler/cqid.test.ts
+++ src/scheduler/cqid.test.ts
@@ -1,6 +1,5 @@
-import { createHash } from 'node:crypto';
-
 import test from 'ava';
+import { NIL as NIL_UUID } from 'uuid';
 
 import { createColumn } from '../schema/column.js';
 import { addCQIDsColumns, cqIDColumn } from '../schema/meta.js';
@@ -10,21 +9,49 @@ import { createTable } from '../schema/table.js';
 import { setCQId } from './cqid.js';
 
 test('setCQId - should set to random value if deterministicCQId is false', (t): void => {
-  const resource = new Resource(addCQIDsColumns(createTable({ name: 'table1' })), null, null);
+  const resource = new Resource(
+    addCQIDsColumns(
+      createTable({
+        name: 'table1',
+        columns: [
+          createColumn({ name: 'pk1', primaryKey: true, unique: true, notNull: true }),
+          createColumn({ name: 'pk2', primaryKey: true, unique: true, notNull: true }),
+          createColumn({ name: 'pk3', primaryKey: true, unique: true, notNull: true }),
+          createColumn({ name: 'non_pk' }),
+        ],
+      }),
+    ),
+    null,
+    null,
+  );
 
-  setCQId(resource, false, () => 'random');
+  setCQId(resource, false, () => NIL_UUID);
 
   t.is(resource.getColumnData(cqIDColumn.name).valid, true);
-  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), 'random');
+  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), NIL_UUID);
 });
 
 test('setCQId - should set to random value if deterministicCQId is true and table does not have non _cq_id PKs', (t): void => {
-  const resource = new Resource(addCQIDsColumns(createTable({ name: 'table1' })), null, null);
+  const resource = new Resource(
+    addCQIDsColumns(
+      createTable({
+        name: 'table1',
+        columns: [
+          createColumn({ name: 'pk1', primaryKey: false, unique: true, notNull: true }),
+          createColumn({ name: 'pk2', primaryKey: false, unique: true, notNull: true }),
+          createColumn({ name: 'pk3', primaryKey: false, unique: true, notNull: true }),
+          createColumn({ name: 'non_pk' }),
+        ],
+      }),
+    ),
+    null,
+    null,
+  );
 
-  setCQId(resource, true, () => 'random');
+  setCQId(resource, true, () => NIL_UUID);
 
   t.is(resource.getColumnData(cqIDColumn.name).valid, true);
-  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), 'random');
+  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), NIL_UUID);
 });
 
 test('setCQId - should set to fixed value if deterministicCQId is true and table has non _cq_id PKs', (t): void => {
@@ -49,16 +76,8 @@ test('setCQId - should set to fixed value if deterministicCQId is true and table
   resource.setColumData('pk3', 'pk3-value');
   resource.setColumData('non_pk', 'non-pk-value');
 
-  const expectedSha256 = createHash('sha256');
-  expectedSha256.update('pk1');
-  expectedSha256.update('pk1-value');
-  expectedSha256.update('pk2');
-  expectedSha256.update('pk2-value');
-  expectedSha256.update('pk3');
-  expectedSha256.update('pk3-value');
-
   setCQId(resource, true);
 
   t.is(resource.getColumnData(cqIDColumn.name).valid, true);
-  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), expectedSha256.digest('hex'));
+  t.is(resource.getColumnData(cqIDColumn.name).value.toString(), '415bd5dd-9bac-5806-b9d1-c53f17d37455');
 });
