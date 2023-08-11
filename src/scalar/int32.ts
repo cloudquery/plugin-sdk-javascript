e35350f3ae76a22f9da3706c305a06561cccd373
Kemal Hadimli <disq@users.noreply.github.com>
2023-08-11 11:57:22 +0100

    Revert "Remove redundant ints"
    This reverts commit 49b6d6f0dfeb84e448f62a3129fd1700f01dee19.

---
+++ src/scalar/int32.ts
@@ -0,0 +1,72 @@
+import { DataType, Int32 as ArrowInt32 } from '@apache-arrow/esnext-esm';
+
+import { Scalar } from './scalar.js';
+import { isInvalid, NULL_VALUE } from './util.js';
+
+export class Int32 implements Scalar<bigint> {
+  private _valid = false;
+  private _value: bigint = BigInt(0);
+
+  public constructor(v?: unknown) {
+    this.value = v;
+    return this;
+  }
+
+  public get dataType(): DataType {
+    return new ArrowInt32();
+  }
+
+  public get valid(): boolean {
+    return this._valid;
+  }
+
+  public get value(): bigint {
+    return this._value;
+  }
+
+  public set value(value: unknown) {
+    if (isInvalid(value)) {
+      this._valid = false;
+      return;
+    }
+
+    if (value instanceof Int32) {
+      this._valid = value.valid;
+      this._value = value.value;
+      return;
+    }
+
+    if (typeof value === 'bigint') {
+      if (!this.validInt32(value)) {
+        throw new TypeError(`Value '${value}' cannot be safely converted to Int32`);
+      }
+      this._value = value;
+      this._valid = true;
+      return;
+    }
+
+    if (typeof value === 'number') {
+      const v = BigInt(value);
+      if (!this.validInt32(v)) {
+        throw new TypeError(`Value '${value}' cannot be safely converted to Int32`);
+      }
+      this._value = v;
+      this._valid = true;
+      return;
+    }
+
+    throw new Error(`Unable to set '${value}' as Int32`);
+  }
+
+  public toString() {
+    if (this._valid) {
+      return String(this._value);
+    }
+
+    return NULL_VALUE;
+  }
+
+  validInt32(n: bigint) {
+    return Number.isSafeInteger(n) && n >= -2_147_483_648 && n <= 2_147_483_647;
+  }
+}
---
+++ src/scalar/uint16.ts
@@ -0,0 +1,72 @@
+import { DataType, Uint16 as ArrowUint16 } from '@apache-arrow/esnext-esm';
+
+import { Scalar } from './scalar.js';
+import { isInvalid, NULL_VALUE } from './util.js';
+
+export class Uint16 implements Scalar<bigint> {
+  private _valid = false;
+  private _value: bigint = BigInt(0);
+
+  public constructor(v?: unknown) {
+    this.value = v;
+    return this;
+  }
+
+  public get dataType(): DataType {
+    return new ArrowUint16();
+  }
+
+  public get valid(): boolean {
+    return this._valid;
+  }
+
+  public get value(): bigint {
+    return this._value;
+  }
+
+  public set value(value: unknown) {
+    if (isInvalid(value)) {
+      this._valid = false;
+      return;
+    }
+
+    if (value instanceof Uint16) {
+      this._valid = value.valid;
+      this._value = value.value;
+      return;
+    }
+
+    if (typeof value === 'bigint') {
+      if (!this.validUint16(value)) {
+        throw new TypeError(`Value '${value}' cannot be safely converted to Uint16`);
+      }
+      this._value = value;
+      this._valid = true;
+      return;
+    }
+
+    if (typeof value === 'number') {
+      const v = BigInt(value);
+      if (!this.validUint16(v)) {
+        throw new TypeError(`Value '${value}' cannot be safely converted to Uint16`);
+      }
+      this._value = v;
+      this._valid = true;
+      return;
+    }
+
+    throw new Error(`Unable to set '${value}' as Uint16`);
+  }
+
+  public toString() {
+    if (this._valid) {
+      return String(this._value);
+    }
+
+    return NULL_VALUE;
+  }
+
+  validUint16(n: bigint) {
+    return Number.isSafeInteger(n) && n >= 0 && n <= 65535;
+  }
+}
